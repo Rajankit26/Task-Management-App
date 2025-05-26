432072ff03239655c3056b7e815448a4b1e1a824
Ankit <rajankit26smp@gmail.com>
2025-05-26 14:57:46 +0530

    created the login form UI

---
+++ frontend/Task-Manager/src/App.jsx
@@ -0,0 +1,46 @@
+
+import React from "react"
+import {BrowserRouter as Router, 
+  Routes,
+  Route
+} from "react-router-dom"
+import Login from "./pages/Auth/Login"
+import Dashboard from "./pages/Admin/Dashboard"
+import ManageTask from "./pages/Admin/ManageTasks"
+import CreateTask from "./pages/Admin/CreateTask"
+import ManageUsers from "./pages/Admin/ManageUsers"
+import UserDashboard from "./pages/User/UserDashboard"
+import MyTasks from "./pages/User/MyTasks"
+import ViewTaskDetails from "./pages/User/ViewTaskDetails"
+import PrivateRoute from "./routes/PrivateRoutes"
+import SignUp from "./pages/Auth/SignUp"
+function App() {
+
+  return (
+   <div>
+    <Router>
+      <Routes>
+        <Route path="/login" element = {<Login />} />
+        <Route path="/signup" element = {<SignUp />} />
+
+        {/* Admin Routes */}
+        <Route element = {<PrivateRoute allowedRoles= {["admin"]} />}>
+        <Route path="/admin/dashboard" element = {<Dashboard />} />
+        <Route path="/admin/tasks" element = {<ManageTask />} />
+        <Route path="/admin/create-task" element = {<CreateTask />} />
+        <Route path="/admin/users" element = {<ManageUsers />} />
+        </Route>
+
+        {/* User Routes */}
+        <Route element = {<PrivateRoute allowedRoles= {["admin"]} />}>
+        <Route path="/user/dashboard" element = {<UserDashboard />} />
+        <Route path="/user/my-tasks" element = {<MyTasks />} />
+        <Route path="/user/task-details/:id" element = {<ViewTaskDetails />} />
+        </Route>
+      </Routes>
+    </Router>
+   </div>
+  )
+}
+
+export default App
