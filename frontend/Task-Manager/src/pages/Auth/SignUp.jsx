2f9c591fb3ea0b04dc137a9109283b44afae0b64
Ankit <rajankit26smp@gmail.com>
2025-05-27 12:45:32 +0530

    build the signup UI form

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,98 @@
-import React from "react";
-
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector"
+import Input from "../../components/Inputs/input";
+import { Link , useNavigate} from "react-router-dom";
 const SignUp = () =>{
+    const [profilePic, setProfilePic] = useState(null)
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const [adminInviteToken, setAdminInviteToken] = useState("")
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+        // Handle SignUp form submit
+        const handleSignUp = async (e) => {
+            e.preventDefault();
+    
+            if(!fullName){
+                setError("Please enter your full name")
+                return;
+            }
+            
+            if(!validateEmail(email)){
+                setError("Please enter a valid email address")
+                return
+            }
+            if(!password){
+                setError("Please enter the password");
+                return;
+            }
+    
+            setError("");
+    
+            // SignUp API call
+        }
+
     return (
-        <div>SignUp</div>
+        <AuthLayout>
+            <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex-col justify-center">
+                <h3 className="text-xl" font-semibold= "true" text-black= "true">Create an Account</h3>
+                <p className="text-xs text-slate-700 mt-[5px] mb-6">Join us today by entering your details below.
+
+                </p>
+
+                <form onSubmit={handleSignUp}>
+                    <ProfilePhotoSelector image= {profilePic} setImage = {setProfilePic}></ProfilePhotoSelector>
+                    <div className="grid grid-cols-1 md:grid:cols-2 gap-4">
+                        <Input 
+                        value = {fullName}
+                        onChange = { ({target}) => setFullName(target.value)}
+                        label= "Full Name"
+                        placeholder = "John"
+                        type="text"
+                        />
+                         <Input 
+                        value = {email} 
+                        onChange = {( {target}) => setEmail(target.value)}
+                        label="Email Address"
+                        placeholder="john@example.com"
+                        type="text"
+                        />
+                        </div>
+                    <div className="grid grid-cols-1 md:grid:cols-2 gap-4">
+                        <Input 
+                        value = {password} 
+                        onChange = {( {target}) => setPassword(target.value)}
+                        label="Password"
+                        placeholder="Min 8 Characters"
+                        type="password"
+                        />
+                        <Input 
+                        value = {adminInviteToken} 
+                        onChange = {( {target}) => setAdminInviteToken(target.value)}
+                        label="Admin Invite Token"
+                        placeholder="6 Digit code"
+                        type="text"
+                        />
+                    </div>  
+                        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+                        <button type = "submit" className="btn-primary">
+                            SIGN UP
+                        </button>
+
+                        <p className="text-[13px] text-slate-800 mt-3">
+                            Already an account?{" "}
+                            <Link className = "font-medium text-orange-500 underline" to= "/login">
+                            Login
+                            </Link>
+                        </p>
+                </form>
+            </div>
+        </AuthLayout>
     )
 }
 
