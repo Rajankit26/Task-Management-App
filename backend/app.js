6238859a71f3257eccf65c773fd98171430299e2
Ankit <rajankit26smp@gmail.com>
2025-07-01 18:28:41 +0530

    Backend: latest updates before pushing to main

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors"
+import  authRoutes  from "./routes/authRoutes.js"
+import  userRoutes  from "./routes/userRoutes.js"
+import  path from "path"
+import { fileURLToPath } from "url";
 
 const app = express()
 
@@ -11,4 +15,12 @@ app.get('/', (req, res) => {
     res.send('Hello from server')
 }) 
 
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 export default app;
