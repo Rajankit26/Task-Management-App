7ae9b2a66ab1fe01a3cce61773cc3f782d5acd53
Ankit <rajankit26smp@gmail.com>
2025-07-02 15:38:43 +0530

    some changes in backend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,7 @@
 import express from "express";
 import cors from "cors"
-import  authRoutes  from "./routes/authRoutes.js"
-import  userRoutes  from "./routes/userRoutes.js"
-import  path from "path"
-import { fileURLToPath } from "url";
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
@@ -15,12 +10,5 @@ app.get('/', (req, res) => {
     res.send('Hello from server')
 }) 
 
-const __fileName = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__fileName);
-
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-
-// Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 export default app;
