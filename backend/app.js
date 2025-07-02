e69c849c6abbca157b980001d2dbef7f0e433f4a
Ankit <rajankit26smp@gmail.com>
2025-07-02 14:51:56 +0530

    Serve static files from /uploads route

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from "url";
 
 const app = express()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
@@ -10,5 +14,7 @@ app.use(express.urlencoded({extended : true}))
 app.get('/', (req, res) => {
     res.send('Hello from server')
 }) 
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 export default app;
